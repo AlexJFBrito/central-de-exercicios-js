02f6b7dff62b705c47dd75cd4e71c13600e21110
Alex <alexjose1995@icloud.com>
2025-04-03 21:54:49 -0300

    js

---
+++ condicionais.js
@@ -0,0 +1,41 @@
+// let numero = prompt("Digite um numero");
+
+
+// if (numero > 0) {
+
+ //   console.log("Positivo.");
+
+//} else {
+
+ //   if (numero < 0) {
+
+  //      console.log("Negativo.");
+
+  //  } else {
+   //     console.log("È zero.");
+
+//    }
+
+// }
+
+let numero = prompt("Digite um numero");
+
+if (numero % 2 ==0  ) {
+
+    console.log("O numero inserido é PAR");
+
+    } else {
+
+        console.log("O numero inserido é IMPAR")
+    }
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,51 @@
-alert ("Hello World na tela ! ") ;
+//alert ("Hello World na tela ! ") ;
 
-console.log ("Hello World no console!");
+//console.log ("Hello World no console!");
+
+let nome = "Alex"; // Pode ser alterado depois
+const idade = 29 ;  // Não pode ser alterado
+var cidade = "Barreiros-PE"; // Não recomendado
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+console.log("Meu nome é: " + nome); //Concatenaçao
+console.log("Minha idade é: " + idade); //Concatenaçao
+console.log("Sou natural de: " + cidade); //Concatenaçao
+
+
+
+
+
+
+
+
+
+console.log("Tenho " + idade  +  " anos" ); //Concatenaçao
+
+ 
+
+console.log(`Nasci em ${cidade} `); // Interpolacao
+
+
+let texto = "Tenho 2 celulares" ;
+let numero = 20 ;
+
+
+console.log(texto) ;
+console.log(numero) ;
+
+
+console.log(typeof idade) ;
+
+let n1 = 10
+let n2 = 10
+
+let soma = n1 + n2;
+
+console.log(soma);
+
+
+
+
